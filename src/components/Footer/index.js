3b98e90c356f10948080b9a0a1dd9aeb9d98758b
staceycami <79605228+staceycami@users.noreply.github.com>
2021-09-16 16:00:36 -0500

    Add files via upload

---
+++ src/components/Footer/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaWhatsapp,
+  FaTwitter
+  
+} from 'react-icons/fa';
+import {
+  FooterContainer,
+  FooterWrap,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcons,
+  SocialIconLink
+} from './FooterElements';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to='/'>Pizza</SocialLogo>
+            <SocialIcons>
+              <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Youtube'>
+                <FaWhatsapp />
+              </SocialIconLink>
+              <SocialIconLink
+                href=''
+                target='_blank'
+                aria-label='Twitter'
+                rel=''
+              >
+                <FaTwitter />
+              </SocialIconLink>            
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
